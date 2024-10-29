a506003d63a41174906645cb7c9de99c25293f73
João Paes <126814093+xuizi@users.noreply.github.com>
2024-10-28 22:01:08 -0300

    Redirecionamento tela /home

--- projeto_web_II/src/app/global_components/solicitar-orcamento-table/solicitar-orcamento-table.component.ts
+++ projeto_web_II/src/app/global_components/solicitar-orcamento-table/solicitar-orcamento-table.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-solicitar-orcamento-table',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './solicitar-orcamento-table.component.html',
   styleUrls: ['./solicitar-orcamento-table.component.scss'],
 })
